79da819a6ae60438a1a900f08a5f97a945ae70d4
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-02 12:50:33 +0300

    completed all task

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,18 @@
-import sum from '../basic';
+import Character from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
-
-  expect(result).toBe(6);
+describe('check character.prototype.damage', () => {
+  test('the value of health should decrease', () => {
+    const expected = 76;
+    const test1 = new Character('gleb', 'sword');
+    test1.damage(40);
+    const received = test1.health;
+    expect(received).toEqual(expected);
+  });
+  test(`the value of health should be 0 `, () => {
+    const expected = 0;
+    const test1 = new Character('gleb', 'sword');
+    test1.damage(9999);
+    const received = test1.health;
+    expect(received).toEqual(expected);
+  })
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,21 @@
-// TODO: write your code here
-import sum from './basic';
+function Character(name, type) {
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.attack = 10;
+  this.defence = 40;
 
-console.log('worked');
+  Character.prototype.damage = function (points) {
+    if (this.health >= 0) this.health -= points * (1 - this.defence / 100);
+    if (this.health <= 0) this.health = 0;
+  };
+}
 
-console.log(sum([1, 2]));
+export default Character;
+
+console.log(`ggdkgh`)
+
+const test1 = new Character('gleb', 'sword');
+console.log(test1);
+test1.damage(9999);
+console.log(test1.health);
